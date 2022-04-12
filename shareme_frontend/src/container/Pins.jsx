445f14679d32b6b7a611db6d8fc7ece6f86cc620
michaelmarcucci <64811901+michaelmarcucci@users.noreply.github.com>
2022-04-11 22:56:10 -0400

    another progress update after health hiatus

--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Routes, Route, NavLink } from 'react-router-dom'
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components'
 
-const Pins = () => {
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
-    <div>Pins</div>
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div>
+        <Routes>
+          <Route path='/' element={<Feed />} />
+          <Route path='/category/:categoryId' element={<Feed />} />
+          <Route path='/pin-detail/:oibId' element={<PinDeatil user={user} />} />
+          <Route path='/' element={<Feed />} />
+          <Route path='/' element={<Feed />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
